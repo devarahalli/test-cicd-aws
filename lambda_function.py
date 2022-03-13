e6eb3bc0d9582cdfe87d2fae15be803a03a91a3c
P D <pd@Pind.local>
2022-03-13 15:58:12 +0530

    New lambda function for CICD

---
+++ lambda_function.py
@@ -0,0 +1,18 @@
+import json 
+import requests
+
+
+def lambda_handler(event, context):
+    
+    ipAddr =  event['requestContext']['identity']['userAgent']
+    #ipAddr = '34.200.210.86' 
+    req = 'http://worldtimeapi.org/api/ip/' + ipAddr + '.json' 
+    res = requests.get(req)
+    body = res.json()
+    body['version'] = "v1"
+    
+     # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps(body)
+    }
